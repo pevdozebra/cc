81c60f8fafe596a1ec2291007b0702462ee552fb
Ilya <ilia992@gmail.com>
2021-01-29 16:37:14 +0300

    CC-3 Добавлена базовая сущность для заказа

---
+++ market-service/src/main/java/co/sptnk/service/model/Order.java
@@ -0,0 +1,33 @@
+package co.sptnk.service.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Product")
+    @SequenceGenerator(name = "Product", sequenceName = "product_id_seq", allocationSize = 1)
+    private Long id;
+
+    /**
+     * Идентификатор заказчика
+     */
+    private Long customerId;
+
+    /**
+     * Идентификатор исполнителя
+     */
+    private Long performerId;
+}
