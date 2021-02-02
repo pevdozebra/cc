895e4009407435c1d8395f252a6ba1646b04346b
Ilya <ilia992@gmail.com>
2021-02-02 10:18:07 +0300

    CC-3 В сущности для заказов и продуктов добавлены поля deleted с значением по умолчанию. В сущность для продуктов добавлен признак активности

--- market-service/src/main/java/co/sptnk/service/model/Order.java
+++ market-service/src/main/java/co/sptnk/service/model/Order.java
@@ -3,31 +3,43 @@ package co.sptnk.service.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name="orders")
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Product")
-    @SequenceGenerator(name = "Product", sequenceName = "product_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Order")
+    @SequenceGenerator(name = "Order", sequenceName = "orders_id_seq", allocationSize = 1)
     private Long id;
 
     /**
      * Идентификатор заказчика
      */
+    @Column(name="customer_id")
     private Long customerId;
 
     /**
      * Идентификатор исполнителя
      */
+    @Column(name="performer_id")
     private Long performerId;
+
+    /**
+     * Признак удаленного заказа
+     */
+    private Boolean deleted = false;
+
+
 }
--- market-service/src/main/java/co/sptnk/service/model/Product.java
+++ market-service/src/main/java/co/sptnk/service/model/Product.java
@@ -3,23 +3,41 @@ package co.sptnk.service.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name="products")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Product")
-    @SequenceGenerator(name = "Product", sequenceName = "product_id_seq", allocationSize = 1)
+    @SequenceGenerator(name = "Product", sequenceName = "products_id_seq", allocationSize = 1)
     private Long id;
 
+    /**
+     * Индектификатор пользователя, которому принадлежит продукт
+     */
+    @Column(name="user_id")
     private Long userId;
+
+    /**
+     * Признак удаленного продукта
+     */
+    private Boolean deleted = false;
+
+    /**
+     * Признак активного продукта
+     */
+    private Boolean active = true;
 }
