f6187a47cf7610362dac25c865cb1680d68d5b2b
Ilya <ilia992@gmail.com>
2021-02-02 10:16:09 +0300

    CC-3 Добавлен класс исключений для генерации ошибок получения данных контроллерами

---
+++ market-service/src/main/java/co/sptnk/service/exceptions/MarketServiceException.java
@@ -0,0 +1,10 @@
+package co.sptnk.service.exceptions;
+
+/**
+ * Исключение для сервиса заказов и продуктов
+ */
+public class MarketServiceException extends Exception{
+    public MarketServiceException(String errorMessage) {
+        super(errorMessage);
+    }
+}
