84ebf8a727919a6e3c801b915d42d25ffaf39945
Ilya <ilia992@gmail.com>
2021-02-17 09:53:39 +0300

    CC-1 Правки получения сообщений из очереди

--- logging-service/src/main/java/co/sptnk/service/services/impl/EventLogReceiver.java
+++ logging-service/src/main/java/co/sptnk/service/services/impl/EventLogReceiver.java
@@ -3,32 +3,31 @@ package co.sptnk.service.services.impl;
 import co.sptnk.lib.common.AMQPKeys;
 import co.sptnk.lib.common.eventlog.EventLogDTO;
 import co.sptnk.service.model.EventLog;
-import co.sptnk.service.repositories.EventLogRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Slf4j
 @Component
+@RabbitListener(queues = AMQPKeys.QUEUE_EVENTLOG)
 public class EventLogReceiver {
 
-    @Autowired
-    private ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
 
-    @Autowired
-    private EventLogRepo eventLogRepo;
+    private final EventLogService eventLogService;
 
-    @RabbitListener(queues = AMQPKeys.QUEUE_EVENTLOG)
+    public EventLogReceiver(ModelMapper mapper, EventLogService service) {
+        this.eventLogService = service;
+        this.modelMapper = mapper;
+    }
+
+    @RabbitHandler
     public void receiveMessage(EventLogDTO eventlogDTO) {
         EventLog eventLog = modelMapper.map(eventlogDTO, EventLog.class);
-        eventLog.setId(UUID.randomUUID());
-        if( eventLog.getEventDate() == null)
-            eventLog.setEventDate(LocalDateTime.now());
-        eventLogRepo.save(eventLog);
+        eventLogService.save(eventLog).subscribe(r -> {
+            log.info("Объект " + r.getId() + " сохранен в БД");
+        });
     }
 }
