9c920569b7017a63ccb911d6cb9e897737f8f427
Ilya <ilia992@gmail.com>
2021-02-17 09:53:17 +0300

    CC-1 Добавлен асинхронный метод отпрвления сообщений в очередь

--- market-service/src/main/java/co/sptnk/service/MarketServiceApplication.java
+++ market-service/src/main/java/co/sptnk/service/MarketServiceApplication.java
@@ -2,7 +2,9 @@ package co.sptnk.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class MarketServiceApplication {
 
---
+++ market-service/src/main/java/co/sptnk/service/common/MessageProducer.java
@@ -0,0 +1,33 @@
+package co.sptnk.service.common;
+
+import co.sptnk.lib.common.AMQPKeys;
+import co.sptnk.lib.common.eventlog.EventCode;
+import co.sptnk.lib.common.eventlog.EventLogDTO;
+import co.sptnk.lib.common.eventlog.EventType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.AsyncRabbitTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class MessageProducer {
+
+    private final AsyncRabbitTemplate rabbitTemplate;
+
+    public MessageProducer(AsyncRabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Async
+    public void sendLogMessage(EventCode code, EventType type, String description) {
+        EventLogDTO eventLog = new EventLogDTO();
+        eventLog.setCode(code);
+        eventLog.setType(type);
+        eventLog.setDescription(description);
+        eventLog.setEventDate(LocalDateTime.now());
+        rabbitTemplate.convertSendAndReceive(AMQPKeys.TOPIC_EVENTLOG, AMQPKeys.QUEUE_EVENTLOG, eventLog);
+    }
+}
