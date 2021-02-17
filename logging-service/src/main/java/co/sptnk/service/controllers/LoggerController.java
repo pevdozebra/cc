aaec066e0565e2805ebe95bf5d6a0bfcd659a85d
Ilya <ilia992@gmail.com>
2021-02-17 13:59:52 +0300

    merge

--- logging-service/src/main/java/co/sptnk/service/controllers/LoggerController.java
+++ logging-service/src/main/java/co/sptnk/service/controllers/LoggerController.java
@@ -5,6 +5,7 @@ import co.sptnk.service.services.impl.EventLogService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.util.Map;
 
 
 @RequestMapping("eventlog")
+@Tag(name = "LoggerController", description = "API объекта EventLog")
 @RestController
 public class LoggerController {
 
